b13d42c583f499d962755ee525778c686ee62592
vkavith <vkavith@gmail.com>
2024-11-17 11:39:04 -0500

    Data Manipulation Part1

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const n4 = 5;
 // Check one: add up to 50
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
-const isSum50 = (n1 + n2 + n3 + n4) == 50;
-    
+const isSum50 = n1 + n2 + n3 + n4 == 50;
+
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
@@ -21,41 +21,51 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-
+console.log(isOver25);
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
-const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+const isUnique =
+  n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
-// Here, we put the results into a single variable 
+// Here, we put the results into a single variable
 // for convenience. Note how we negate isOver25 using
-// the ! operator. We could also have tested for 
+// the ! operator. We could also have tested for
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
 // Finally, log the results.
 console.log(`${isValid}`);
 
-let divisibleByFive = (n1%5==0) &&(n2%5==0) && (n3%5 ==0) && (n4%5 ==0);
-  console.log("is all divisible by 5 " , divisibleByFive);
+let divisibleByFive = n1 % 5 == 0 && n2 % 5 == 0 && n3 % 5 == 0 && n4 % 5 == 0;
+console.log("is all divisible by 5 ", divisibleByFive);
 
 //check if first number larer than last
-let isFoverL = (n1>4);
-   console.log('is first greate than last' , isFoverL);
+let isFoverL = n1 > n4;
+console.log("is first greater than last", isFoverL);
 
-let arithChain = ((n2 - n1) * n3 ) % n4;
-   console.log('arithmetic chain' , arithChain);
+let arithChain = ((n2 - n1) * n3) % n4;
+console.log("arithmetic chain", arithChain);
 
 let isOver25change = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
-
+console.log("Is over 25", isOver25change);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
 // we break things into small, manageable pieces so that
 // they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
-  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+const dontDoThis =
+  n1 + n2 + n3 + n4 == 50 &&
+  (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2 &&
+  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) &&
+  n1 != n2 &&
+  n1 != n3 &&
+  n1 != n4 &&
+  n2 != n3 &&
+  n2 != n4 &&
+  n3 != n4;
+
+
+  
